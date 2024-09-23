09af997655b3bda8d3ca558084e2f03e391e1308
krutika jadhav <krutikajadhav012@gmail.com>
2024-09-23 18:08:12 +0530

    banner updated

---
+++ demo.js
@@ -0,0 +1,18 @@
+<div data-elementor-type="wp-post" data-elementor-id="2" class="elementor elementor-2"
+									data-elementor-post-type="page">
+									<div class="elementor-element elementor-element-ab4b3e5 e-con-full animated-slow e-flex e-con e-parent"
+										data-id="ab4b3e5" data-element_type="container" data-settings='{
+         "background_background": "slideshow",
+         "background_slideshow_gallery": [
+             { "url": "../assets/images/9.jpg" },
+             { "id": 4613, "url": "assets/images/Banner 1.png" },
+             { "id": 4610, "url": "assets/images/Banner 2.png" }
+         ],
+         "background_slideshow_slide_duration": 1500,
+         "background_slideshow_ken_burns": "yes",
+         "animation": "none",
+         "background_slideshow_transition_duration": 1000,
+         "background_slideshow_loop": "yes",
+         "background_slideshow_slide_transition": "fade",
+         "background_slideshow_ken_burns_zoom_direction": "in"
+     }'></div>
